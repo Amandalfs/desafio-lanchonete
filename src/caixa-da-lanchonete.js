fb219cd72e6f081583716b860327cfc14a200669
Amandalfs <amandalfs57@gmail.com>
2024-06-03 18:44:33 -0300

    parte de taxa e calculo de total de compra(teste)

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,7 +1,26 @@
+import { Carrinho } from "./carrinho.js";
+
 class CaixaDaLanchonete {
     calcularValorDaCompra(metodoDePagamento, itens) {
+        const descontoComDinheiro = 0.05;
+        const crescimoComCredito = 0.03;
 
+        const carrinho = new Carrinho(itens)
+        let totalDaCompra =  carrinho.calcularValorTotal()
+
+        if(metodoDePagamento === 'dinheiro'){
+            return totalDaCompra -= totalDaCompra * descontoComDinheiro;
+        } else if(metodoDePagamento === 'credito'){
+            return totalDaCompra += totalDaCompra * crescimoComCredito;
+        }
+        
+        return totalDaCompra;
     }
+
+
 }
+
+const caixa = new CaixaDaLanchonete()
+console.log(caixa.calcularValorDaCompra('dinheiro', ['sanduiche,1', 'cafe,1']))
 
 export { CaixaDaLanchonete };
---
+++ src/item.js
@@ -0,0 +1,11 @@
+export class Item {
+    codigo;
+    quantidade;
+    preco; 
+
+    constructor(codigo, quantidade, preco){
+        this.codigo = codigo;
+        this.quantidade = quantidade;
+        this.preco = preco
+    }
+}
