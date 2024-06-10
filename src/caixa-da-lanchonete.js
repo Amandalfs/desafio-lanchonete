99cd4d9a4f5aa56c4acfa4390c5cfc4c95c15b8c
Amandalfs <amandalfs57@gmail.com>
2024-06-10 17:48:23 -0300

    Remember

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,20 +1,6 @@
-import { Carrinho } from "./carrinho.js";
-
 class CaixaDaLanchonete {
     calcularValorDaCompra(metodoDePagamento, itens) {
-        const descontoComDinheiro = 0.05;
-        const crescimoComCredito = 0.03;
-
-        const carrinho = new Carrinho(itens)
-        let totalDaCompra =  carrinho.calcularValorTotal()
-
-        if(metodoDePagamento === 'dinheiro'){
-            return totalDaCompra -= totalDaCompra * descontoComDinheiro;
-        } else if(metodoDePagamento === 'credito'){
-            return totalDaCompra += totalDaCompra * crescimoComCredito;
-        }
-        
-        return totalDaCompra;
+       
     }
 
 
